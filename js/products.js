51748e41704cbe14fe7ba501facd09fe3378e607
dragotaadelin <dragotaadelin@gmail.com>
2021-09-06 12:07:27 +0300

    4th page changes

--- js/products.js
+++ js/products.js
@@ -1,6 +1,8 @@
 const main = document.querySelector(".main");
 const icons = main.getElementsByTagName("i");
 const filters = main.getElementsByClassName("filter-categories");
+const backgrounds = main.getElementsByClassName("background");
+const buttons = main.getElementsByTagName("button");
 
 for (let i = 0; i < icons.length; i++) {
   icons[i].addEventListener("click", () => {
@@ -8,3 +10,14 @@ for (let i = 0; i < icons.length; i++) {
     icons[i].classList.toggle("fa-arrow-circle-up");
   });
 }
+
+// for (let i = 0; i < backgrounds.length; i += 2) {
+//   backgrounds[i].addEventListener("mouseover", () => {
+//     buttons[i].classList.toggle("hidden");
+//     buttons[i + 1].classList.toggle("hidden");
+//   });
+//   backgrounds[i].addEventListener("mouseleave", () => {
+//     buttons[i].classList.toggle("hidden");
+//     buttons[i + 1].classList.toggle("hidden");
+//   });
+// }
